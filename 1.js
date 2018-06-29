d3e564a960447c6a581dc6096879e89859e78ea1
minh <yeumaianh198@gmail.com>
2018-06-29 18:33:34 +0700

    HELLO

--- 1.js
+++ 1.js
@@ -1,13 +1,26 @@
-$(function() {
-	$('.nd1nut').slideUp();
-	$('.motnut h3').mouseover(function(event) {
-		/* Act on the event */
-		$('.nd1nut').slideUp();
-		$(this).next().slideDown();
-		
-	});
-	
 
-	
 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    rtl:true,
+    nav:true,
+    loop:true,
+    margin:10,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
 });
+
+    owl.trigger('play.owl.autoplay',[3000])
